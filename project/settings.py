06f50b06f7efaf336aec94d76a5b4ccda568c87b
Roger Wells <rsw359@gmail.com>
2022-06-03 10:20:55 +0900

    all done except docker and deployment

--- project/settings.py
+++ project/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
     "corsheaders",
     # local
     "accounts",
-    "cookies",
+    "cookie_stands",
 ]
 
 MIDDLEWARE = [
@@ -169,5 +169,5 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
 CSRF_TRUSTED_ORIGINS = [
-    ‘https://cookie-stand-api-ms.herokuapp.com’
+    'https://cookie-stand-api-ms.herokuapp.com'
 ]
